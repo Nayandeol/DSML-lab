0a057119f070b678846fe80fda52f89777fb5b59
Nayandeol <149756825+Nayandeol@users.noreply.github.com>
2024-08-14 11:32:18 +0530

    Add files via upload

---
+++ matrix_trans.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=[]
+size=int(input("enter the size of the array: "))
+for i in range(size):
+	row=list(map(int,input().split()))
+	arr.append(row)
+im=np.transpose(arr)
+print("inverse: ",im)	
